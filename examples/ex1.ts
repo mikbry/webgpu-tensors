15564b56d1debc29e4d6919278ddb8c735a5a465
mikbry <mik@miklabs.com>
2024-09-27 15:45:25 +0200

    feat: Add new tensor operations and print examples

--- examples/ex1.ts
+++ examples/ex1.ts
@@ -3,6 +3,12 @@ import t from '../src/webgpu-tensors'
 let x = await t.rand([5, 3]);
 await t.print(x);
 
+x = await t.ones([5, 3]);
+await t.print(x);
+
+x = await t.rand([5, 3]);
+await t.print(x);
+
 x = await t.tensor([0, 1, 2, 3]);
 await t.print(x);
 
@@ -19,6 +25,7 @@ const tensor = await t.rand([3,4]);
 t.print(`Shape of tensor: ${tensor.shape}`);
 t.print(`Datatype of tensor: ${tensor.dtype}`);
 t.print(`Device tensor is stored on: ${tensor.device}`);
+await t.print("tensor", tensor, x);
 
 x = await t.empty([3, 4, 5]);
 t.print(x.size());
@@ -30,3 +37,7 @@ const y = await t.tensor([[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]
 await t.print('matmul', x, y);
 const result = await t.matmul(x, y);
 t.print('matmul', x, y, result);
+
+x = await t.tensor([-2, -1, 0, 1, 2, 3]);
+const max = await t.maximum(x, 0);
+await t.print("maximum=", max);
