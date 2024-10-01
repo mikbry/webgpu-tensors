370a3bbd75b7aade4b0bf98a8b1745fdb325d440
mikbry <mik@miklabs.com>
2024-10-01 13:12:30 +0200

    feat: Refactor WebGPUTensors to use a singleton pattern

--- tests/tensors.test.ts
+++ tests/tensors.test.ts
@@ -1,18 +1,18 @@
 import { expect, test, describe, beforeEach } from 'vitest';
-import { WebGPUTensors, Tensor, DType } from '../src/webgpu-tensors';
+import t, { Tensors, DType } from '../src/webgpu-tensors';
 
 describe('WebGPUTensors', () => {
-  let tensors: WebGPUTensors;
+  let tensors: Tensors;
 
   beforeEach(async () => {
-    tensors = new WebGPUTensors();
-    await tensors.init();
+    tensors = t;
+    await t.init();
   });
 
   test('Create empty tensor', () => {
     const shape = [2, 3];
     const tensor = tensors.empty(shape);
-    expect(tensor).toBeInstanceOf(Object); // Changed from Tensor to Object
+    expect(tensor).toBeInstanceOf(Object);
     expect(tensor.shape.data).toEqual(shape);
     expect(tensor.dtype).toBe(DType.Float32);
   });
@@ -20,24 +20,24 @@ describe('WebGPUTensors', () => {
   test('Create ones tensor', async () => {
     const shape = [2, 2];
     const tensor = tensors.ones(shape);
-    expect(tensor).toBeInstanceOf(Object); // Changed from Tensor to Object
+    expect(tensor).toBeInstanceOf(Object);
     expect(tensor.shape.data).toEqual(shape);
-    const data = await tensor.readFloat32();
-    expect(data).toEqual(new Float32Array([1, 1, 1, 1]));
+    const data = await t.clone(tensor).readArray<number>();
+    expect(data).toEqual([[1, 1], [1, 1]]);
   });
 
   test('Matrix multiplication', async () => {
     const a = tensors.tensor([[1, 2], [3, 4]]);
     const b = tensors.tensor([[5, 6], [7, 8]]);
     const result = tensors.matmul(a, b);
-    const data = await result.readFloat32();
-    expect(Array.from(data)).toEqual([19, 22, 43, 50]);
+    const data = await t.clone(result).readArray<number>();
+    expect(data).toEqual([[19, 22], [43, 50]]);
   });
 
   test('ReLU activation', async () => {
     const input = tensors.tensor([-2, -1, 0, 1, 2]);
     const result = tensors.relu(input);
-    const data = await result.readFloat32();
-    expect(Array.from(data)).toEqual([0, 0, 0, 1, 2]);
+    const data = await t.clone(result).readFloat32();
+    expect(Array.from(data as Float32Array)).toEqual([0, 0, 0, 1, 2]);
   });
 });
