811ad909ffb572a83c5cc0d9a149db7d0cea1683
mikbry <mik@miklabs.com>
2024-10-04 16:08:13 +0200

    feat: Remove unused imports from WASM library

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 #[cfg(target_arch = "wasm32")]
-use webgpu_tensors::{RSTensors, Tensors, RSTensor, TensorOptions, Device, DType};
+use webgpu_tensors::{RSTensors, Tensors, RSTensor, TensorOptions};
 #[cfg(target_arch = "wasm32")]
 use serde::{Serialize, Deserialize};
 #[cfg(target_arch = "wasm32")]
