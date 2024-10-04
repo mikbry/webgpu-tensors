a38a9f861bebb1e06c6d2319fd154243b1563072
mikbry <mik@miklabs.com>
2024-10-04 15:04:21 +0200

    feat: add serde dependency and tensor operations

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,7 +1,14 @@
 #[cfg(target_arch = "wasm32")]
-use wasm_bindgen::{prelude::wasm_bindgen, JsCast, UnwrapThrowExt};
+use wasm_bindgen::{prelude::wasm_bindgen, JsCast, JsValue, UnwrapThrowExt};
 #[cfg(target_arch = "wasm32")]
-use webgpu_tensors::{RSTensors, Tensors};
+use webgpu_tensors::{RSTensors, Tensors, RSTensor, Shape, TensorOptions};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct JSTensorOptions {
+    dtype: Option<String>,
+    device: Option<String>,
+}
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
@@ -41,8 +48,32 @@ impl WASMTensorsImpl {
         self.instance.destroy();
     }
 
-    #[inline]
-    pub fn randn(&mut self) {
-        // TODO
+    #[wasm_bindgen]
+    pub fn randn(&self, shape: Vec<usize>, options: JsValue) -> Result<JsValue, 
+JsValue> {
+        let js_options: JSTensorOptions = 
+options.into_serde().unwrap_or(JSTensorOptions { dtype: None, device: None });
+        let tensor_options = TensorOptions {
+            dtype: js_options.dtype.map(|d| d.parse().unwrap()),
+            device: js_options.device.map(|d| d.parse().unwrap()),
+        };
+        let tensor = self.instance.randn(shape, Some(tensor_options));
+        Ok(JsValue::from_serde(&tensor).unwrap())
+    }
+
+    #[wasm_bindgen]
+    pub fn matmul(&self, a: JsValue, b: JsValue) -> Result<JsValue, JsValue> {
+        let tensor_a: RSTensor = a.into_serde().unwrap();
+        let tensor_b: RSTensor = b.into_serde().unwrap();
+        let result = self.instance.matmul(&tensor_a, &tensor_b);
+        Ok(JsValue::from_serde(&result).unwrap())
+    }
+
+    #[wasm_bindgen]
+    pub fn mul(&self, a: JsValue, b: JsValue) -> Result<JsValue, JsValue> {
+        let tensor_a: RSTensor = a.into_serde().unwrap();
+        let tensor_b: RSTensor = b.into_serde().unwrap();
+        let result = self.instance.mul(&tensor_a, &tensor_b);
+        Ok(JsValue::from_serde(&result).unwrap())
     }
 }
