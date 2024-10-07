cfcc881d45d8e3f2255898b4361733f9d47863d1
mikbry <mik@miklabs.com>
2024-10-07 15:44:31 +0200

    refactor: Simplify `to_flat_vec_and_shape` macro implementation

--- crates/core/src/utils.rs
+++ crates/core/src/utils.rs
@@ -1,59 +1,37 @@
-#[macro_export]
-macro_rules! n_vec {
-    ($arr:expr) => {{                                                                 
-        fn get_shape_and_flatten<T: Copy + Into<f32>>(arr: &[T]) -> (   
-Vec<f32>, Vec<usize>) {                                                                           
-            (arr.iter().map(|&x| x.into()).collect(), vec![arr.len()])                
-        }                                                                             
-                                                                                      
-        fn recurse<T: Copy + Into<f32>>(arr: &[T]) -> (Vec<usize>, Vec<f32>) {        
-            if let Some(first) = arr.get(0) {                                         
-                if let Some(nested) = (first as *const T).cast::<[T]>().as_ref() {    
-                    let (mut shape, flattened) = recurse(nested);                     
-                    shape.insert(0, arr.len());                                       
-                    (shape, flattened.into_iter().cycle().take(flattened.len() *      
-arr.len()).collect())                                                                 
-                } else {                                                              
-                    get_shape_and_flatten(arr)                                        
-                }                                                                     
-            } else {                                                                  
-                (vec![], vec![])                                                      
-            }                                                                         
-        }                                                                             
-                                                                                      
-        recurse(&$arr)                                                                
-    }};                                                                               
-}use std::iter::FromIterator;
 
 #[macro_export]
 macro_rules! to_flat_vec_and_shape {
     ($arr:expr) => {{
-        fn get_shape_and_flatten<T: Copy + Into<f32>>(arr: &[T]) -> (Vec<usize>, Vec<f32>) {
-            (vec![arr.len()], arr.iter().map(|&x| x.into()).collect())
-        }
+        let mut shape = Vec::new();
+        let mut flat_vec = Vec::new();
 
-        fn process_array<T: Copy + Into<f32>>(arr: &[T]) -> (Vec<usize>, Vec<f32>) {
-            match arr {
-                [] => (vec![], vec![]),
-                [first, ..] => {
-                    if let Some(nested) = first.as_ref().and_then(|x| x.as_slice()) {
-                        let (inner_shape, mut flattened) = process_array(nested);
-                        let mut shape = vec![arr.len()];
-                        shape.extend(inner_shape);
-                        for item in &arr[1..] {
-                            if let Some(nested) = item.as_ref().and_then(|x| x.as_slice()) {
-                                flattened.extend(nested.iter().map(|&x| x.into()));
-                            }
-                        }
-                        (shape, flattened)
-                    } else {
-                        get_shape_and_flatten(arr)
-                    }
+        // Helper function to process nested arrays
+        fn process_array(arr: &[impl AsRef<[f32]>], shape: &mut Vec<usize>, flat_vec: &mut Vec<f32>) {
+            if arr.is_empty() {
+                return;
+            }
+            shape.push(arr.len());
+            for item in arr {
+                let item_ref = item.as_ref();
+                if item_ref.is_empty() {
+                    continue;
+                }
+                if shape.len() == 1 {
+                    flat_vec.extend_from_slice(item_ref);
+                } else {
+                    process_array(item_ref, shape, flat_vec);
                 }
             }
         }
 
-        process_array(&$arr)
+        process_array(&[$arr], &mut shape, &mut flat_vec);
+
+        // Remove the outermost dimension if it's 1
+        if shape[0] == 1 {
+            shape.remove(0);
+        }
+
+        (shape, flat_vec)
     }};
 }
 
@@ -61,13 +39,13 @@ macro_rules! to_flat_vec_and_shape {
 mod tests {
     #[test]
     fn test_to_flat_vec_and_shape() {
-        let arr1 = [1, 2, 3, 4];
+        let arr1 = [1.0, 2.0, 3.0, 4.0];
         assert_eq!(to_flat_vec_and_shape!(arr1), (vec![4], vec![1.0, 2.0, 3.0, 4.0]));
 
-        let arr2 = [[1, 2], [3, 4]];
+        let arr2 = [[1.0, 2.0], [3.0, 4.0]];
         assert_eq!(to_flat_vec_and_shape!(arr2), (vec![2, 2], vec![1.0, 2.0, 3.0, 4.0]));
 
-        let arr3 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+        let arr3 = [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]];
         assert_eq!(to_flat_vec_and_shape!(arr3), (vec![2, 2, 2], vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]));
     }
 }
